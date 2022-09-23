9135dcae628925846f9817cf85d4e49cc0373fa1
AlejandraSan <alejandrade10x10@hotmail.com>
2022-09-23 17:50:19 -0300

    backend, spring

---
+++ MiProyecto/BackEnd/src/test/java/com/portfolio/alejandra/AlejandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.alejandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlejandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
