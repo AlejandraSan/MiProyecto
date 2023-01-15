d2f6a8a642c28e51b68dde79e5273c1ed84a141b
AlejandraSan <alejandrade10x10@hotmail.com>
2023-01-15 19:37:20 -0300

    cambio

--- MiProyecto/FrontEnd/src/app/service/persona.service.ts
+++ MiProyecto/FrontEnd/src/app/service/persona.service.ts
@@ -27,9 +27,7 @@ export class PersonaService {
     return this.httpClient.get<persona>(this.URL + `detail/${id}`);
   }
 
- /* public save(educacion: Educacion): Observable<any>{
-    return this.httpClient.post<any>(this.URL + 'create', educacion);
-  }*/
+ 
 
   public update(id: number, Persona: persona): Observable<any>{
     return this.httpClient.put<any>(this.URL + `update/${id}`, Persona);
